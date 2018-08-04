13775a4878f917a80578fe90fe6dce7718b40385
tomohiro.isobe <isobe.tomohiro@gmail.com>
2018-08-04 18:22:05 +0900

    DesignComplete

---
+++ app/src/main/java/jp/techacademy/isobe/tomohiro/calcapp/MainActivity.java
@@ -0,0 +1,32 @@
+package jp.techacademy.isobe.tomohiro.calcapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(this);
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+
+        Button button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(this);
+
+        Button button4 = (Button) findViewById(R.id.button4);
+         button4.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
